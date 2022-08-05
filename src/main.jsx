427c84f813228bb133cab54d836c8323e98f5302
filipe <filipe.ogramlich@gmail.com>
2022-08-04 23:10:57 -0300

    theme-colors-created

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Details} from './pages/Details'
+import {Details} from './pages/index'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
