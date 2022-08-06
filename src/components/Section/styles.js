a7f85ec5eecb1ab62d9e9e4b3902d4bd4119e6dd
filipe <filipe.ogramlich@gmail.com>
2022-08-06 12:14:49 -0300

    section-children-created

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -20,3 +20,17 @@ export const Container = styled.section`
     
     }
 ` 
+
+export const Links = styled.ul`
+
+    list-style: none;
+
+    >li{
+        margin-top: 12px;
+
+    }
+    a {
+        color:${({theme}) =>theme.COLORS.WHITE} ;
+
+    }
+`
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,11 +4,22 @@ import { Button } from "../components/Button"
 
 import { Container } from "./styles"
 
+import { Section } from "../components/Section"
+
+import { Links } from "../components/Section/styles"
+
 export function Details(){
 
   return(
     <Container>
       <Header/>
+      <Section title="Links Úteis">
+        <Links>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          <li><a href="#">https://www.rocketseat.com.br/</a></li>
+        </Links>
+      </Section>
+
       <Button text = "Voltar"/>      
     </Container>
   )
