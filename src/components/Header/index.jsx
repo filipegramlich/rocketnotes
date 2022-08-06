5e4f787b255ec6e18fbf43bcf3df570f0c8fd402
filipe <filipe.ogramlich@gmail.com>
2022-08-05 22:40:11 -0300

    header-icon-added

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,22 @@
-import { Container } from "./styles";
+import {RiShutDownLine} from "react-icons/ri"
+
+import { Container, Profile, Logout} from "./styles";
 
 export function Header(){
     return(
         <Container>
-            
+            <Profile>
+                <img src="https://github.com/filipegramlich.png" alt="User Image" />
+                <div>
+                    <span>Bem vindo</span>
+                    <strong>Filipe Gramlich</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
+
         </Container>
     )
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -16,3 +16,48 @@ export const Container = styled.header`
 
     padding: 0 80px;
 `
+export const Profile = styled.div`
+
+    display: flex;
+    align-items: center;
+
+    >img{
+
+        width: 56px;
+        height: 56px;
+        border-radius: 50%;
+    }
+
+    >div{
+
+        display: flex;
+        flex-direction: column;
+
+        margin-left: 16px;
+        line-height: 24px;
+
+
+
+        span{
+            font-size: 14px;
+            color:${({theme}) => theme.COLORS.GRAY_100 } ;
+            
+        }
+        strong{
+            font-size: 18px;
+            color:${({theme}) => theme.COLORS.WHITE} ;
+            
+        }
+    }
+`;
+
+export const Logout = styled.button`
+    border: none;
+    background: none;
+
+    >svg{
+        color:${({theme}) => theme.COLORS.GRAY_100} ;
+        font-size: 36px;
+    }
+
+`
