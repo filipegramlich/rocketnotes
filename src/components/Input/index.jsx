8a2625dc56b5735e7e904af5e76e07274191de79
filipe <filipe.ogramlich@gmail.com>
2022-08-09 05:57:49 -0300

    input-component-finalized

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,8 +1,12 @@
 import { Container } from "./styles";
 
-export function Input(){
+export function Input( {icon: Icon, ...rest}){
     return(
         <Container>
+
+            {Icon && <Icon size={20}/>}
+            <input {...rest }/>
+
             
         </Container>
     )
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,12 @@
 import {FiPlus} from "react-icons/fi"
+import {FiSearch} from "react-icons/fi"
 
 import { Container, Brand, Menu, Search, Content, Newnote} from "./styles"
 
 import { Header } from "../../components/Header";
+
+import { Input } from "../../components/Input";
+
 
 import { ButtonText } from "../../components/ButtonText";
 
@@ -29,7 +33,7 @@ export function Home() {
             </Menu>
 
             <Search>
-
+                <Input placeholder="Pesquisar pelo titulo" icon={FiSearch}/>
             </Search>
 
             <Content>
