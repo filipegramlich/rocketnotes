bcc06895db5dc88ed7a766b14d29f5bd970d8c39
filipe <filipe.ogramlich@gmail.com>
2022-09-07 19:49:31 -0300

    new-textarea-created

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -2,6 +2,7 @@ import { Header } from '../../components/Header'
 import { Container } from './styles'
 import { Form } from './styles'
 import {Input } from '../../components/Input'
+import {Textarea } from '../../components/Textarea'
 
 export function New(){
     return(
@@ -17,6 +18,10 @@ export function New(){
 
                     <Input
                         placeholder="Título"
+                    />
+
+                    <Textarea
+                        placeholder="Observações"
                     />
 
                     
